ccfbdd02e1d5806d1148d81c9149cdbb67c19202
maris <maris.raukas@voco.ee>
2025-02-17 13:58:32 +0200

    Add player and its game logic

---
+++ game.py
@@ -0,0 +1,21 @@
+import pygame
+from settings import Settings
+from player import Player
+import game_functions as gf
+
+def run_game():
+    pygame.init()
+    gm_settings = Settings()
+
+    screen = pygame.display.set_mode([gm_settings.screen_width, gm_settings.screen_height])
+    pygame.display.set_caption(gm_settings.caption)
+    
+    player = Player(screen)
+    
+    while True:
+        gf.check_events(player)
+        player.update()
+        gf.update_screen(gm_settings, screen, player)
+                
+        
+run_game()
---
+++ settings.py
@@ -0,0 +1,11 @@
+class Settings:
+    """Class for game settings"""
+    
+    
+    def __init__(self):
+        """initialize game settings"""
+        #screen settings
+        self.screen_width = 800
+        self.screen_height = 600
+        self.bg_color = (0, 0, 139)
+        self.caption = "Bubble Bluster"
